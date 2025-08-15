783113d138f00919c6d78bf7739464455c1974ac
Chatchai-29 <jjunejune7799@gmail.com>
2025-08-15 21:20:14 +1000

    tailwind-edit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.css"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
