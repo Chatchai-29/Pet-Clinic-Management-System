118be0c1f8b3545437ff102bb23fd3dfe651f2a0
Chatchai-29 <jjunejune7799@gmail.com>
2025-08-18 15:35:58 +1000

    Edit IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.62.227.183', // live
+  //baseURL: 'http://52.62.227.183', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
